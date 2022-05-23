66129ca46159682abb7bbd75862c9b453a62abf7
roynirmal <nroy000@gmail.com>
2022-05-23 16:11:47 +0200

    localStorage -> sessionStorage

--- standard/js/queries.js
+++ standard/js/queries.js
@@ -24,10 +24,10 @@ var tasks = [
 
 document.addEventListener("DOMContentLoaded", function(){
     let querybox, answer, need, item, a;
-    let fv = localStorage.getItem('first-visit')
-    let qv = localStorage.getItem('ques-visit')
+    let fv = sessionStorage.getItem('first-visit')
+    let qv = sessionStorage.getItem('ques-visit')
     let currenTask;
-    let cp = localStorage.getItem('current_page');
+    let cp = sessionStorage.getItem('current_page');
     let viewedList = window.sessionStorage.getItem('viewedList');
     viewedList = JSON.parse(viewedList);
     
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function(){
         item = JSON.parse(sessionStorage.getItem('task'))
         need = document.getElementById('info');
         need.textContent = item.info
-        localStorage.setItem('ques-visit', 1) 
+        sessionStorage.setItem('ques-visit', 1) 
         // console.log("q", need)
     } else if( fv == 1 && cp != "question") {
         // cookie doesn't exist, create it now
@@ -54,18 +54,18 @@ document.addEventListener("DOMContentLoaded", function(){
         let a = sessionStorage.getItem('answer')
         answer.textContent = item.answer[parseInt(a)]    
     } else if ( qv == 1 && cp == "question" && fv ==1) {
-        localStorage.setItem('ques-visit', 0) 
+        sessionStorage.setItem('ques-visit', 0) 
         window.location.href = './question.html'
     } else {
         // console.log("first")
-        localStorage.setItem('current_page', 'template')
+        sessionStorage.setItem('current_page', 'template')
         let pages = JSON.parse(sessionStorage.getItem('pages'))
         
         
        
         
-            localStorage.setItem('first-visit', 1)
-            localStorage.setItem('ques-visit', 0) 
+            sessionStorage.setItem('first-visit', 1)
+            sessionStorage.setItem('ques-visit', 0) 
             var mynum_index = Math.floor(Math.random() * pages.length); 
             item = tasks[pages[mynum_index]];
     
