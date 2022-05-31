4fb292cf31e02961d29542351d6144dda9872961
roynirmal <nroy000@gmail.com>
2022-05-31 23:48:56 +0200

    separate file for tasks

--- standard/js/queries.js
+++ standard/js/queries.js
@@ -1,39 +1,7 @@
-var tasks = [
-    {   
-        id: "rem_short_1",
-        query: "what metal are hip replacements made of",
-        answer: [ "Some prosthesis, like hip and knee joints made of cobalt chrome, contain some trace of nickel and for patients with allergies to this may have to go with Titanium joints.",
-        "What are the different types of hip replacement surgery? Types of hip surgery include: 1  total hip replacement. 2  metal-on-metal hip resurfacing."],
-        info: "You want to find out what metal are hip replacements made of"
-    }, {
-        id: "rem_short_2",
-        query: "who formed the commonwealth of independent states",
-        answer: [ "Commonwealth of Independent States (CIS) Alliance of 12 of the former republics of the Soviet Union (Armenia, Azerbaijan, Belarus, Georgia, Kazakstan, Kyrgyzstan, Moldova, Russia, Tajikistan, Turkmenistan, Ukraine, and Uzbekistan)",
-        "'Commonwealth of Independent States; putative successor to the USSR, except that the Baltic Republics of Estonia, Latvia, and Lithuania never joined. "],
-        info: "You want to find out which countries formed the commonwealth of independent states"
-    },
-    {
-        id: "und_short_1",
-        query: "medicare's definition of mechanical ventilation",
-        answer: [ "Mechanical ventilation is the medical term for artificial ventilation where mechanical means is used to assist or replace spontaneous breathing.",
-        "Physiology Explains CPAP’s Effectiveness. Non-invasive pressure-support ventilation (NIPSV), a method of assisting a patient’s respiration without intubation, was first reported in the 18th century."],
-        info: "You want to understand the definition of mechanical ventilation according to medicare"
-    },
-    {
-        id: "und_short_2",
-        query: "define visceral?",
-        answer: [ "Definition of visceral for English Language Learners. : 1  coming from strong emotions and not from logic or reason. 2  medical : of or relating to the viscera.",
-        "A viscero-visceral reflex is a reflex in which stimulation of visceral receptors results in activation of visceral motor phenomena"],
-        info: "You want to understand the definition of visceral in english language"
-    },
-    {
-        id: "und_short_3",
-        query: "lps laws definition",
-        answer: [ "The Lanterman–Petris–Short (LPS) Act concerns the involuntary civil commitment to a mental health institution in the State of California. The act set the precedent for modern mental health commitment procedures in the United States.",
-        "When a patient has a serious psychiatric disorder or mental health problem, he may be admitted to a mental hospital, also referred to as a psychiatric facility, for evaluation and treatment."],
-        info: "You want to understand the definition of LPS laws"
-    }
-]
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const tasks = require('./task_data/tasks.js')
+var tasks = data;
 
 document.addEventListener("DOMContentLoaded", function(){
     let querybox, answer, need, item, a;
