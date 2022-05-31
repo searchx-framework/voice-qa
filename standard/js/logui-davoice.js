d7787888fdc68df63eb197e5b713ca90e08b56a1
roynirmal <nroy000@gmail.com>
2022-05-31 12:00:18 +0200

    added logui code

---
+++ standard/js/logui-davoice.js
@@ -0,0 +1,57 @@
+
+let logUIConfigurationObject = {
+    logUIConfiguration: {
+        endpoint: 'ws://logui.ewi.tudelft.nl/ws/endpoint/',
+        authorisationToken: null,
+        verbose: true, // Turn this off when the experiment is running properly! Set it to false.
+    },
+    applicationSpecificData: {
+        userID: null, // To be overwritten by the driver script.
+        mode: null,
+        task: null,
+        answer: null,
+    },
+    browserEvents: {
+        trackCursor: false,
+        contextMenu: true,
+        pageFocus: true,
+        pageResize: true,
+        eventsWhileScrolling: false,
+    },
+    trackingConfiguration: {
+        'rel-click': {
+            selector: 'div.relevance div input',  // Maybe we can make this a bit cleaner. Need to make this bubble up for capturing the tab name, metadata!
+            event: 'click',
+            name: 'REL_CLICK',
+            metadata: [
+                {
+                    nameForLog: 'relLabel',
+                    sourcer: 'elementAttribute',
+                    lookFor: 'value',
+                },
+            ],
+        },
+        'ans-click': {
+            selector: 'div.answer-card > div.flex-container > div.left-container > span.answer button',  // Maybe we can make this a bit cleaner. Need to make this bubble up for capturing the tab name, metadata!
+            event: 'click',
+            name: 'ANSWER_CLICK'
+        },
+        'ans-hover': {
+            selector: 'div.answer-card > div.flex-container > div.left-container > span.answer button',  // Maybe we can make this a bit cleaner. Need to make this bubble up for capturing the tab name, metadata!
+            event: 'mouseHover',
+            properties: {
+                mouseenter: {
+                    name: 'ANS_HOVERIN',
+                },
+                mouseleave: {
+                    name: 'ANS_HOVEROUT',
+                }
+            },
+        },
+        'next-click': {
+            selector: 'main > div > div > div.left-container div button',  // Maybe we can make this a bit cleaner. Need to make this bubble up for capturing the tab name, metadata!
+            event: 'click',
+            name: 'NEXT_CLICK'
+        },
+    }
+}
