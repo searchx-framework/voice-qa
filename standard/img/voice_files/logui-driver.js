e45a9602ec0b8b388e96f840bf54e6c0526dba18
roynirmal <nroy000@gmail.com>
2022-06-03 17:28:34 +0200

    tasks, audio, landing changes

---
+++ standard/img/voice_files/logui-driver.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    // When we get here, the page is ready for LogUI to be started.
+    let prolificID;
+    prolificID = sessionStorage.getItem('prolific-id') || null;
+    let mode = sessionStorage.getItem('mode') || null;
+    let task = JSON.parse(sessionStorage.getItem('task')).id || null;
+    let answer = sessionStorage.getItem('answer') || null;
+
+    if (!prolificID) {
+        prolificID = extractProlificID();   
+    }
+        
+
+    // if (!prolificID & serp) {
+    //     alert("NOOOOO");
+    //     window.location = '/landing.html'; // Redirect if there is no ID.
+    // }
+
+    // Set up the config object for this run.
+    logUIConfigurationObject.applicationSpecificData.userID = prolificID;
+    logUIConfigurationObject.applicationSpecificData.mode = mode;
+    logUIConfigurationObject.applicationSpecificData.task = task;
+    logUIConfigurationObject.applicationSpecificData.answer = answer;
+    logUIConfigurationObject.logUIConfiguration.authorisationToken = 'eyJ0eXBlIjoibG9nVUktYXV0aG9yaXNhdGlvbi1vYmplY3QiLCJhcHBsaWNhdGlvbklEIjoiOWJkZGYwZTQtYzU4Yy00YTkwLTk0ZjEtMzk4NGMxNjlkNmRiIiwiZmxpZ2h0SUQiOiIyYzBlYzQxYy04MWZiLTQ4MjMtYWE1Ni1jMWY0ZTQwOTZhZTQifQ:1nvwfQ:ta5kpaHXeJ35s1xscNOTWH_fsHWYirzw_Nbgi7zYXXM';
+    
+    LogUI.init(logUIConfigurationObject);
+});
+
+// Example STOP of LogUI - just add the conditional and the stop() API call to wherever you need it to stop.
+document.addEventListener("unload", function() {
+    if (LogUI.isActive()) {
+        LogUI.stop();
+    }
+})
+
+// Adapted from https://stackoverflow.com/a/2091331
+function extractProlificID() {
+    var variable = 'prolificID';
+    
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+
+    return null;
+}
---
+++ standard/img/voice_files/queries.js
@@ -0,0 +1,92 @@
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+// const tasks = require('./task_data/tasks.js')
+var tasks = data;
+
+document.addEventListener("DOMContentLoaded", function(){
+    let querybox, answer, need, item, a;
+    let fv = sessionStorage.getItem('first-visit')
+    let qv = sessionStorage.getItem('ques-visit')
+    let currenTask;
+    let cp = sessionStorage.getItem('current_page');
+    let viewedList = window.sessionStorage.getItem('viewedList');
+    viewedList = JSON.parse(viewedList);
+    // console.log("FV", typeof(fv))
+
+
+    if( fv == 1) {
+        // console.log("refresh")
+        querybox = document.getElementById('query');
+        answer = document.getElementById('answer')
+        // need = document.getElementById('info');
+        let item = JSON.parse(sessionStorage.getItem('task'))
+        querybox.value = item.query
+        // need.textContent = item.info  
+
+        let a = sessionStorage.getItem('answer')
+        var audio1= document.getElementById("audio1")
+        console.log("V", '../voice_answers/' + item.id + '_' + a +'.mp3')
+        answer.src = '../voice_answers/' + item.id + '_' + a +'.mp3'
+        audio1.load();
+        
+        // answer.src = '../voice_answers/' + item.id + '_' + a +'.mp3'
+
+    } else {
+        sessionStorage.setItem('current_page', 'template')
+        let pages = JSON.parse(sessionStorage.getItem('pages'))
+        sessionStorage.setItem('first-visit', 1)
+        sessionStorage.setItem('ques-visit', 0) 
+        // let cp = sessionStorage.getItem('current_page');
+        
+
+            var mynum_index = Math.floor(Math.random() * pages.length); 
+            item = tasks[pages[mynum_index]];
+
+            sessionStorage.setItem('task', JSON.stringify(item))
+        
+        
+        
+            querybox = document.getElementById('query');
+            // let answer = document.getElementById('answer');
+            // need = document.getElementById('info');
+            answer = document.getElementById('answer')
+        
+            
+            
+            
+            querybox.value = item.query
+            // need.textContent = item.info
+            a = Math.floor(Math.random() * 3)
+            sessionStorage.setItem('answer', a)
+            var audio1= document.getElementById("audio1")
+            // console.log("V", '../voice_answers/' + item.id + '_' + a +'.mp3')
+            answer.src = '../voice_answers/' + item.id + '_' + a +'.mp3'
+            audio1.load();
+            
+        
+            pages.splice(mynum_index , 1)
+            
+            sessionStorage.setItem('pages', JSON.stringify(pages))
+        
+    }
+
+    // console.log("Test", querybox.value, answer) 
+
+    // for (let component of scrollableComponents) {
+    //     console.log(component)
+    //     // initScrollableComponent(component);
+    // }
+});
+
+// function initScrollableComponent(element) {
+//     for (let component of element.getElementsByTagName('span')) {
+//         switch (component.className) {
+//             case 'left':
+//                 component.getElementsByTagName('a')[0].addEventListener('click', scrollLeft);
+//                 break;
+//             case 'right':
+//                 component.getElementsByTagName('a')[0].addEventListener('click', scrollRight);
+//                 break;
+//         }
+//     }
+// }
