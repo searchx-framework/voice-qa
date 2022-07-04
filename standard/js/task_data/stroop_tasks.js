1cb8ed696432ce6dc8c36171d707cee4709bb735
roynirmal <nroy000@gmail.com>
2022-07-04 16:16:26 +0200

    ospan, connecting all together

---
+++ standard/js/task_data/stroop_tasks.js
@@ -0,0 +1,257 @@
+var stroop_data = [
+    {
+        "text": "Red",
+        "color": "blue",
+        "hex": "#0000FF", 
+        "type": "incong"
+    },     {
+        "text": "Yellow",
+        "color": "blue",
+        "hex": "#0000FF", 
+        "type": "incong"
+    },    {
+        "text": "Green",
+        "color": "blue",
+        "hex": "#0000FF", 
+        "type": "incong"
+    }, 
+    {
+        "text": "Red",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "incong"
+    },    {
+        "text": "Blue",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "incong"
+    },    {
+        "text": "Yellow",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "incong"
+    },  {
+        "text": "Red",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "incong"
+    },  {
+        "text": "Blue",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "incong"
+    },  {
+        "text": "Green",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "incong"
+    },  {
+        "text": "Yellow",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },  {
+        "text": "Blue",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },  {
+        "text": "Green",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },   {
+        "text": "Red",
+        "color": "blue",
+        "hex": "#0000FF", 
+        "type": "incong"
+    },     {
+        "text": "Yellow",
+        "color": "blue",
+        "hex": "#0000FF", 
+        "type": "incong"
+    },    {
+        "text": "Green",
+        "color": "blue",
+        "hex": "#0000FF", 
+        "type": "incong"
+    }, 
+    {
+        "text": "Red",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "incong"
+    },    {
+        "text": "Blue",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "incong"
+    },    {
+        "text": "Yellow",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "incong"
+    },  {
+        "text": "Red",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "incong"
+    },  {
+        "text": "Blue",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "incong"
+    },  {
+        "text": "Green",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "incong"
+    },  {
+        "text": "Yellow",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },  {
+        "text": "Blue",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },  {
+        "text": "Green",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },  {
+        "text": "Red",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },     {
+        "text": "Yellow",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Green",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Blue",
+        "color": "blue",
+        "hex": "#000FF", 
+        "type": "cong"
+    }, {
+        "text": "Red",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },     {
+        "text": "Yellow",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Green",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Blue",
+        "color": "blue",
+        "hex": "#000FF", 
+        "type": "cong"
+    }, {
+        "text": "Red",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },     {
+        "text": "Yellow",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Green",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Blue",
+        "color": "blue",
+        "hex": "#000FF", 
+        "type": "cong"
+    }, {
+        "text": "Red",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },     {
+        "text": "Yellow",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Green",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Blue",
+        "color": "blue",
+        "hex": "#000FF", 
+        "type": "cong"
+    }, {
+        "text": "Red",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },     {
+        "text": "Yellow",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Green",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Blue",
+        "color": "blue",
+        "hex": "#000FF", 
+        "type": "cong"
+    }, {
+        "text": "Red",
+        "color": "red",
+        "hex": "#FF0000", 
+        "type": "cong"
+    },     {
+        "text": "Yellow",
+        "color": "yellow",
+        "hex": "#FFFF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Green",
+        "color": "green",
+        "hex": "#00FF00", 
+        "type": "cong"
+    }, 
+    {
+        "text": "Blue",
+        "color": "blue",
+        "hex": "#000FF", 
+        "type": "cong"
+    } 
+]
